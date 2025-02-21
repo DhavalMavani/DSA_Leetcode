ccf0ea5a6e56edb6388c29962ac04de9e443960e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-21 16:07:59 +0530

    Time: 47 ms (29.1%), Space: 77.1 MB (68.28%) - LeetHub

--- 0894-random-pick-with-blacklist/0894-random-pick-with-blacklist.cpp
+++ 0894-random-pick-with-blacklist/0894-random-pick-with-blacklist.cpp
@@ -1,19 +1,49 @@
 class Solution {
 public:
-    vector<int> nums;
-    int s;
-    Solution(int n, vector<int>& blacklist) {
-        n=min((int)1e6,n);
-        unordered_set<int> ust(begin(blacklist), end(blacklist));
-        for(int i=0;i<n;i++) if(!ust.count(i)) nums.emplace_back(i);
-        ust.clear();
-        s=nums.size();
+
+    int approach;
+
+    int n, m;
+
+    unordered_set<int> hash_set;
+
+    vector<int> whitelist;
+
+
+    Solution(int nn, vector<int>& blacklist) {
+        n = nn;
+        m = blacklist.size();
+        for(auto it : blacklist) hash_set.insert(it);
+        if(m * 2 < n) approach = 1;
+        else {
+            approach = 2;
+            for(int i = 0; i < n; i++) {
+                if(hash_set.find(i) == hash_set.end()) {
+                    whitelist.push_back(i);
+                }
+            }
+        }
     }
     
     int pick() {
-        return nums[rand()%s];
+        if(approach == 1) {
+            while(true) {
+                int val = rand() % n;
+                if(!hash_set.count(val) )  return val;
+            }
+        }
+        else {
+            int idx = rand() % whitelist.size();
+            return whitelist[idx];
+        }
     }
 };
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(n, blacklist);
+ * int param_1 = obj->pick();
+ */
 
 /**
  * Your Solution object will be instantiated and called as such:
