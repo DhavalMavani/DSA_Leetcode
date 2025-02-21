a81e07676729985c927aa280b22aef98bdf79bc9
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-21 15:41:11 +0530

    Time: 17 ms (95.48%), Space: 51.9 MB (96.14%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    int bfs(vector<vector<int>> &adj){
-        int dist=0;
-        queue<int> q;
-        q.emplace(0);
-        
-        vector<bool> vis(adj.size(),false);
-        while(!q.empty()){
-            int n=q.size();
-
-            for(int i=0;i<n;i++){
-
-                int curr=q.front();
-                q.pop();
-                vis[curr]=true;
-                if(curr==adj.size()) return dist;
-
-                for(auto &node: adj[curr] ){
-                    if(!vis[node]) q.emplace(node);
-                }
-            }
-            dist++;
-
+    void updateDistanceDfs(int node,vector<vector<int>> &adj,vector<int> &dist){
+        for(auto &i: adj[node]){
+            if(dist[i]<=1+dist[node]) continue;
+            dist[i]= 1+ dist[node];
+            updateDistanceDfs(i, adj, dist);
         }
-        return dist;
     }
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(n-1);
-        for(int i=0;i<n-1;i++) adj[i].emplace_back(i+1);
+        vector<vector<int>> adj(n);
+        vector<int> dist(n);
+        dist[0]=n-1;
+        for(int i=1;i<n;i++){
+            dist[i]=n-i-1;
+            adj[i].emplace_back(i-1);
+        } 
         
         int s=queries.size();
         vector<int> ans(s);
+
         for(int i=0;i<s;i++){
             int n1=queries[i][0], n2=queries[i][1];
-            adj[n1].emplace_back(n2);
+            adj[n2].emplace_back(n1);
 
-            ans[i]=bfs(adj);
+            updateDistanceDfs(n2,adj,dist);
+            ans[i]=dist[0];
         }
         return ans;
     }
