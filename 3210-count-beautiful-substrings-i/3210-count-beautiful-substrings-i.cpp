235851293bea90ba86a4aef879e11bf3a4ffce2f
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-28 15:34:50 +0530

    Time: 34 ms (65.45%), Space: 10 MB (29.87%) - LeetHub

--- 3210-count-beautiful-substrings-i/3210-count-beautiful-substrings-i.cpp
+++ 3210-count-beautiful-substrings-i/3210-count-beautiful-substrings-i.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int beautifulSubstrings(string s, int k) {
         int n=s.size(),ans=0;
-        vector<bool> vowels(26,false);
-        vowels[0]=vowels[4]=vowels[8]=vowels[14]=vowels[20]=true;
         for(int i=0;i<n;i++){
             int vowelCount=0;
             for(int j=i;j<n;j++){
-                if( vowels[s[j]-'a'] ) vowelCount++;
+                if( s[j]=='a'|| s[j]=='e'|| s[j]=='i'|| s[j]=='o'|| s[j]=='u' ) vowelCount++;
                 if((2*vowelCount)==j-i+1 && (vowelCount*vowelCount)%k==0) ans++;
             }
         }
