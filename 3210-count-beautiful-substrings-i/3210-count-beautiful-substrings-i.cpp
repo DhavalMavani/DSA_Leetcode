e673838546c18100ee90ef94e7f8add5343151c2
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-02-28 15:29:25 +0530

    Time: 34 ms (65.45%), Space: 10 MB (48.57%) - LeetHub

--- 3210-count-beautiful-substrings-i/3210-count-beautiful-substrings-i.cpp
+++ 3210-count-beautiful-substrings-i/3210-count-beautiful-substrings-i.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     int beautifulSubstrings(string s, int k) {
         int n=s.size(),ans=0;
-        unordered_set<char> vowel;
-        vowel.emplace('a');
-        vowel.emplace('e');
-        vowel.emplace('i');
-        vowel.emplace('o');
-        vowel.emplace('u');
         for(int i=0;i<n;i++){
             int vowelCount=0;
             for(int j=i;j<n;j++){
-                if(vowel.count( s[j] ) ) vowelCount++;
+                char ch=s[j];
+                if( ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u' ) vowelCount++;
                 if((2*vowelCount)==j-i+1 && (vowelCount*vowelCount)%k==0) ans++;
             }
         }
