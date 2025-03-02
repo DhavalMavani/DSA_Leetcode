5d2afc7c58082ea88aa3f989aa1188b78e84e659
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-02 17:12:27 +0530

    Time: 38 ms (80.51%), Space: 32.5 MB (66.95%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
+    char lower(vector<int> &freq){
+        for(int i=0;i<26;i++) if(freq[i]!=0) return 'a'+i;
+        return 'a';
+    }
+
     string robotWithString(string s) {
-        vector<int> freq(26,0);
-        for(auto &i: s) freq[i-'a']++;
-        stack<char> st;
+        vector<int> freq(26);
+        for(auto &i:s) freq[i-'a']++;
         string ans;
-        int l=0;
-        for(int i=0;i<26;i++){
-            while(!st.empty() && st.top()-'a'<=i){
+        stack<char> st;
+
+        for(auto &ch:s){
+            st.push(ch);
+            freq[ch-'a']--;
+
+            while(st.size() && st.top()<=lower(freq)){
                 ans+=st.top();
                 st.pop();
             }
-
-            while(freq[i]!=0){
-                if(s[l]-'a'==i) ans+=s[l];
-                else st.emplace(s[l]);
-                
-                freq[s[l]-'a']--;
-                l++;
-            }
-
         }
-
-        while(!st.empty()){
+        while(st.size()){
             ans+=st.top();
             st.pop();
         }
-
         return ans;
     }
 };
