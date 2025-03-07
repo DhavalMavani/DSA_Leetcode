a288b1c96ecf86466f4b7058176372f37a5c63ad
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-07 11:31:30 +0530

    Time: 274 ms (37.78%), Space: 14 MB (77.59%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -11,15 +11,16 @@ public:
                 for(int j=2*i; j<=right; j+=i) isPrime[j]=false;
             }
         }
-        int prev=-1;
+        int prev=-1, minDiff=1e9;
         vector<int> ans(2,-1);
         for(int i=left;i<=right;i++){
             if(isPrime[i]){
                 if(prev==-1) prev=i;
                 else{
-                    if(ans[0]==-1 || (ans[1]-ans[0] > i-prev ) ){
+                    if(ans[0]==-1 || minDiff > i-prev ){
                         ans[0]=prev;
                         ans[1]=i;
+                        minDiff=i-prev;
                     }
                     prev=i;
                 }
