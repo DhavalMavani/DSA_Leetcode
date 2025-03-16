3d8ca3e1967836d8634c7525c132582f8140ef6e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-16 21:26:58 +0530

    Time: 15 ms (91.9%), Space: 59.3 MB (45.76%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    bool check(vector<int>& ranks, int cars, long long t){
+        for(auto &r: ranks){
+            cars-= int ( sqrt(t/r) );
+            if(cars<=0) return true;
+        }
+        return false;
+    }
+
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long l=1,r=INT_MIN;
+        for(auto &i: ranks) r=max((long long)i,r);
+
+        r*= ((long long)cars*cars);
+        
+
+        while(l<=r){
+            long long mid=(l+r)/2;
+
+            if(check(ranks, cars, mid) ){
+                r=mid-1;
+            }
+            else l=mid+1;
+        }
+
+        return l;
+    }
+};
