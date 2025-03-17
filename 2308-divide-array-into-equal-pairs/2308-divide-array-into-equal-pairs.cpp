63feba3adf9176f6744de7b8b483f254bb04e4d2
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-17 13:49:26 +0530

    Time: 4 ms (42.39%), Space: 18.2 MB (9.58%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int,int> ump;
-        for(auto &i: nums) ump[i]++;
-
-        for(auto &i: ump) if(i.second%2) return false;
-        return true;
+        unordered_set<int> ust;
+        for(auto &i: nums){
+            if(ust.count(i) ) ust.erase(i);
+            else ust.emplace(i);
+        }
+        return ust.empty();
     }
 };
