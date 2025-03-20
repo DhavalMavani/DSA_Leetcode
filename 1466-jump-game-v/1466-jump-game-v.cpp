67ae3d34d612f77f521424a259a1e1a2886cc350
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-20 13:42:25 +0530

    Time: 13 ms (57.83%), Space: 19 MB (74.49%) - LeetHub

--- 1466-jump-game-v/1466-jump-game-v.cpp
+++ 1466-jump-game-v/1466-jump-game-v.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int helper(int currInd,vector<int>& arr, int &d, vector<int> &dp){
-        int count=0, maxH=arr[currInd];
+        int count=0, maxH=arr[currInd],n=arr.size();
         
         if(dp[currInd]!=-1) return dp[currInd];
-        int lim=min((int) arr.size()-1,currInd+d);
+
+        int lim=min((int) n-1,currInd+d);
         for(int i=currInd+1; i<=lim ;i++ ){
             if(arr[i]>=arr[currInd]) break;
             count=max(count,helper(i,arr,d,dp) );
         }
+        
         lim=max(0,currInd-d);
         for(int i=currInd-1; i>= lim;i-- ){
             if(arr[i]>=arr[currInd]) break;
@@ -18,12 +20,12 @@ public:
         return dp[currInd]=1 + count;
     }
     int maxJumps(vector<int>& arr, int d) {
-        vector<int> dp(arr.size(),-1);
+        int n=arr.size(), ans=1;
 
-        int ans=1;
-        for(int i=0;i<arr.size();i++){
-            ans=max(ans, helper(i,arr,d,dp) );
-        }
+        vector<int> dp(n,-1);
+
+        for(int i=0;i<n;i++) ans=max(ans, helper(i,arr,d,dp) );
+
         return ans;
     }
 };
