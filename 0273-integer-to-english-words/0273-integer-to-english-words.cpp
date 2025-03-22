66aa455d61e79854abe124a471b2ee6a6f42c89d
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-22 15:28:14 +0530

    Time: 0 ms (100%), Space: 10.5 MB (63.11%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    unordered_map<int, string> numToString = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"},
-        {9, "Nine"}, {10, "Ten"}, {11, "Eleven"}, {12, "Twelve"},
-        {13, "Thirteen"}, {14, "Fourteen"}, {15, "Fifteen"},
-        {16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"},
-        {19, "Nineteen"},
-
-        {20, "Twenty"}, {30, "Thirty"}, {40, "Forty"},
-        {50, "Fifty"}, {60, "Sixty"}, {70, "Seventy"},
-        {80, "Eighty"}, {90, "Ninety"}
-    };
+    string ones[20] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    string tens[10] = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
     string helper(int num){
         string ans;
 
-        if(num%100>20 && num%10){
-            ans+= numToString[num%10] ;
-            num-=num%10;
-            if(num%100) ans= numToString [num%100]+" "+ans ; 
+        if(num%100>=20){
+            ans+= ones[num%10] ;
+            num/=10;
+            if(num%10){
+                string temp= tens[num%10];
+                if(!ans.empty()) ans=temp+" "+ans;
+                else ans=temp;
+            } 
         } 
-        else ans= numToString[num%100];
-
-        num/=100;
+        else{
+            ans= ones[num%100];
+            num/=10;
+        }
+        num/=10;
 
         if(num){
-            string temp= numToString[num] + " Hundred";
+            string temp= ones[num] + " Hundred";
             if(!ans.empty()) ans=temp+" "+ans;
             else ans=temp;
         } 
