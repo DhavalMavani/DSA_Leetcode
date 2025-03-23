7a5ed35464ccf6ea5e54959a0b563cb64c6fd058
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-23 11:42:39 +0530

    Time: 7 ms (68.34%), Space: 23.3 MB (76.51%) - LeetHub

--- 1508-longest-happy-prefix/1508-longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/1508-longest-happy-prefix.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    vector<int> calcLPS(string &s){
+
+
+    string longestPrefix(string s) {
         int len=0, n=s.size(),i=1;
         vector<int> LPS(n);
-
         LPS[0]=0;
         while(i<n){
             if(s[len]== s[i] ) LPS[i++]=++len;
@@ -12,12 +13,6 @@ public:
                 else LPS[i++]=0;
             }
         }
-        return LPS;
-    }
-
-    string longestPrefix(string s) {
-        int n=s.size();
-        vector<int> LPS= calcLPS(s);
         return s.substr( n-LPS[n-1],LPS[n-1]);
     }
 };
