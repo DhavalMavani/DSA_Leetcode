3e70760e4c2e4b0128e97012f2f5d5b74f3f81f7
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-24 16:37:52 +0530

    Time: 108 ms (80.23%), Space: 184.5 MB (66.9%) - LeetHub

--- 2832-find-the-longest-equal-subarray/2832-find-the-longest-equal-subarray.cpp
+++ 2832-find-the-longest-equal-subarray/2832-find-the-longest-equal-subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestEqualSubarray(vector<int>& s, int k) {
 
-        int l=0,r=0,res=0,maxFrequency=0;
+        int l=0,r=0,maxFrequency=0;
         unordered_map <int,int> ump;
 
         while(r<s.size()){
@@ -12,7 +12,6 @@ public:
                 ump[s[l]]--;
                 l++;
             }
-            res=max(res,r-l+1);
             r++;
         };
         
