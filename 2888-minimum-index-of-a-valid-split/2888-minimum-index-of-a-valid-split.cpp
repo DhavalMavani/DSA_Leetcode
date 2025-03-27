25a025f6ab27b7038084c09dd4d6a08cf6942318
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-27 12:30:00 +0530

    Time: 37 ms (52.87%), Space: 98.5 MB (60.66%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int dominatrix,n=nums.size(),count=0;
+        if(n==1) return -1;
+        unordered_map<int,int> freq;
+
+        for(int i=0;i<n;i++) freq[nums[i]]++;
+
+        for(auto &i: freq){
+            if(i.second>count){
+                count=i.second;
+                dominatrix=i.first;
+            } 
+        }
+        
+        int left=0;
+        cout<<dominatrix<<" ";
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==dominatrix) left++;
+            if( (left> (i+1)/2) && ( count-left >(n -i-1)/2  ) ) return i;
+        }
+        return -1;   
+    }
+};
