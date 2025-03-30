f1d83a83dad741cbfb29b75517b768ab88b909d3
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-03-31 03:07:51 +0530

    Time: 75 ms (95.43%), Space: 38.4 MB (95.93%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -7,7 +7,6 @@ public:
         for(int i=0;i<k;i++) arr.push_back({queries[i],i});
         sort(arr.begin(),arr.end());
 
-
         vector<bool> vis(m*n,false);
         priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>> > q;
 
@@ -16,28 +15,23 @@ public:
 
         int dirs[5]={0,1,0,-1,0};
         
-        while(!q.empty() && ind<k){
-            auto [val, pos] = q.top();
-            q.pop();
-            while(ind<k && val>=arr[ind].first){
-                ans [arr[ind].second] =count;
-                ind++;
-            }
+        for(auto &[query, ind]: arr ){
+            while(!q.empty() && query>q.top().first){
+                auto [val, pos] = q.top();
+                q.pop();
 
-            count++;
-            for(int i=0;i<4;i++){
-                int x=dirs[i], y=dirs[i+1], r= pos/n, c= pos%n, newPos=(r+x)*n +c+y;
-                if(r+x>=0 && r+x<m && c+y>=0 && c+y<n && !vis[newPos] ){
-                    vis[newPos ]=true;
-                    q.push({grid[r+x][c+y],newPos });
+                count++;
+                for(int i=0;i<4;i++){
+                    int x=dirs[i], y=dirs[i+1], r= pos/n, c= pos%n, newPos=(r+x)*n +c+y;
+                    if(r+x>=0 && r+x<m && c+y>=0 && c+y<n && !vis[newPos] ){
+                        vis[newPos ]=true;
+                        q.push({grid[r+x][c+y],newPos });
+                    }
                 }
             }
+            ans[ind]=count;
         }
 
-        while (ind < k) {
-            ans[arr[ind].second]=count;
-            ind++;
-        }
         return ans;
     }
 };
