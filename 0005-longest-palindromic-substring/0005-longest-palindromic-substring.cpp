d4ce3db2109906abab91955c7d6cdc9b04e13ae7
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-02 18:36:56 +0530

    Time: 198 ms (25.24%), Space: 39.1 MB (31.27%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,18 +1,33 @@
 class Solution {
-public:
-    void palindrome(string &s, int l,int r, pair<int,int> &ansIndex){
-        while(l>=0 && r<s.size() && s[l]==s[r]){
-            if(r-l>ansIndex.second-ansIndex.first) ansIndex={l,r};
-            l--;
-            r++;
+private:
+    bool palindrome(string &s,int &strPos,int &endPos,vector<vector<bool>> &dp){
+        if( s[strPos]==s[endPos] && dp[strPos+1][endPos-1] ){
+            return dp[strPos][endPos]=true;
         }
+        return false;
     }
+public:
     string longestPalindrome(string s) {
-        pair<int,int> ansIndex={0,0};
-        for(int i=0;i<s.size();i++){
-            palindrome(s,i,i,ansIndex);
-            palindrome(s,i,i+1,ansIndex);
+        string ansStr=s.substr(0,1);
+        int n=s.size();
+        vector<vector<bool>> dp(n,vector <bool>(n,false));
+        for(int i=0;i<n;i++) dp[i][i]=true;
+        
+        for(int i=0;i<n-1;i++){
+            if(s[i]==s[i+1]){
+                dp[i][i+1]=true;
+                ansStr=s.substr(i,2);
+            }
         }
-        return s.substr(ansIndex.first,ansIndex.second-ansIndex.first+1);
+
+        for(int len= 3; len<=s.size(); len++){
+            for(int strPos=0;strPos<=s.size()-len;strPos++){
+                int endPos=len+strPos-1;
+                bool a=palindrome(s,strPos,endPos,dp);
+
+                if(a && len>ansStr.size()) ansStr=s.substr(strPos,len);
+            }
+        }
+        return ansStr;
     }
 };
