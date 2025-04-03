a819db4ea58d28bb3d64bf308d9d294c8489bb30
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-03 14:33:30 +0530

    Time: 0 ms (100%), Space: 21.5 MB (90.23%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> stack;
+        int n=asteroids.size();
         
-        for(int i=0;i<asteroids.size();i++){
+        for(int i=0;i<n;i++){
             while(!stack.empty() && (asteroids[i]<0 && stack.back()>0) ){
-                int diff= asteroids[i]+ stack.back();
-                if(diff<0){
-                    stack.pop_back();
-                }
-                else if(diff>0){
-                    asteroids[i]=0;
-                }
+                if(abs(asteroids[i])> stack.back()) stack.pop_back();
                 else{
-                    stack.pop_back();
+                    if(abs(asteroids[i])==stack.back()) stack.pop_back();
                     asteroids[i]=0;
-                }
+                    break;
+                } 
             }
 
-            if(asteroids[i]){
-                stack.emplace_back(asteroids[i]);
-            }
+            if(asteroids[i]) stack.emplace_back(asteroids[i]);
            
-
         }
         
         return stack;
