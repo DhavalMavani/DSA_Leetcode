b443dcc08bf1b992eae89b5090d9ec211b0c0710
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-03 16:49:54 +0530

    Time: 4 ms (36.67%), Space: 18.8 MB (19.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,16 +6,17 @@ public:
     }
 
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n=nums.size();
         
         unordered_map <int,int> ump;
-        for (int i=0; i<nums.size();i++) ump[ nums[i] ] ++;
+        for (int i=0; i<n;i++) ump[ nums[i] ] ++;
 
-        vector <pair<int,int>> v2={};
+        vector <pair<int,int>> v2;
         copy (ump.begin(),ump.end(),back_inserter(v2));
         sort(v2.begin(),v2.end(), comp);
 
         vector <int> ans;
-        for(int i=0; i<k;i++) ans.emplace_back( (*(v2.begin()+i)).first );
+        for(int i=0; i<k;i++) ans.emplace_back( v2[i].first );
 
         return ans;
     }
