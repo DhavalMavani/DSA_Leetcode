3dbd6614fdac1c29796d1da9c5e11cfc315b3f31
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-03 16:52:47 +0530

    Time: 0 ms (100%), Space: 17.7 MB (92.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-
-    static bool comp( pair <int,int> &a, pair <int,int> &b){
-        return a.second>=b.second;
-    }
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n=nums.size();
         
         unordered_map <int,int> ump;
         for (int i=0; i<n;i++) ump[ nums[i] ] ++;
 
-        vector <pair<int,int>> v2;
-        copy (ump.begin(),ump.end(),back_inserter(v2));
-        sort(v2.begin(),v2.end(), comp);
+        priority_queue<pair<int,int>> pq;
+        for(auto &i: ump) pq.push({i.second,i.first});
 
         vector <int> ans;
-        for(int i=0; i<k;i++) ans.emplace_back( v2[i].first );
+        for(int i=0; i<k;i++){
+            ans.emplace_back( pq.top().second );
+            pq.pop();
+        } 
 
         return ans;
     }
