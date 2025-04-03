157b66caa2c13034ee447e8f23aaa0c6c0423db3
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-03 15:35:18 +0530

    Time: 3 ms (89.82%), Space: 120.6 MB (67.07%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
@@ -5,12 +5,10 @@ public:
         int n=nums.size(),mini=INT_MAX, miniInd=INT_MAX;
 
         for(auto &i: nums){
-            // cout<<i%value<<" ";
             int t=i%value;
             if(t<0) t+=value;
             arr[t]++;
         } 
-        // cout<<"\n\n";
 
         for(int i=0;i<value;i++){
             if(arr[i]==0) return i;
