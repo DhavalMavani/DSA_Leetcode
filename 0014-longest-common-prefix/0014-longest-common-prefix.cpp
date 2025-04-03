371888ae0922fd4a0bc69a302e0e9a695bc3a5d2
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-03 18:03:31 +0530

    Time: 0 ms (100%), Space: 11.8 MB (69.79%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,16 +3,14 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string pref=strs[0];
         int n=strs.size(), len=pref.size();
-        for(int i=1;i<n;i++){
-            
-            while(len> strs[i].size()) len--;
+
+        for(int i=1;i<n;i++){    
+
+            len=min(len, (int)strs[i].size());
 
             while(len-1>=0 && pref.substr(0,len) != strs[i].substr(0,len) ) len--;
             if(len==0) return "";
-
-            pref=pref.substr(0,len);
-
         }
-        return pref;
+        return pref.substr(0,len);
     }
 };
