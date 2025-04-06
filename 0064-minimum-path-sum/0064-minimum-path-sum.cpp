92fc0334170681049fc0bf8fa95f5b843b6e16ac
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-06 11:47:22 +0530

    Time: 0 ms (100%), Space: 16.4 MB (13.92%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int calculatePath(vector<vector<int>>& grid,int row,int col,vector<vector<int>>& dp){
-        int rs=grid.size(), cs=grid[0].size();
-        if(row==rs-1 && col==cs-1) return grid[row][col];
+        if(row==grid.size()-1 && col==grid[0].size()-1) return grid[row][col];
 
         if(dp[row][col]!=-1) return dp[row][col];
 
         int ans=10e7;
 
-        if( row+1<rs) ans=min( calculatePath(grid,row+1,col,dp), ans);
+        if( row+1<grid.size()) ans=min( calculatePath(grid,row+1,col,dp), ans);
 
-        if( col+1<cs) ans=min( calculatePath(grid,row,col+1,dp), ans);
+        if( col+1<grid[0].size()) ans=min( calculatePath(grid,row,col+1,dp), ans);
 
         return dp[row][col]=ans+grid[row][col];
     }
