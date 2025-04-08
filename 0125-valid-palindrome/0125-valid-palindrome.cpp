ecd09b093afee2508ef5fbcb9102854ae3260432
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-08 14:59:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (35.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,11 +3,10 @@ public:
     bool isPalindrome(string s) {
         string str;
         for (auto &i: s) if (isalnum(i)) str+=i;
-        transform(str.begin(), str.end(), str.begin(), ::tolower);
 
         long l=0,r=str.size()-1;
         while(l<r){
-            if(str[l]!=str[r]) return false;
+            if(tolower(str[l])!=tolower(str[r]) ) return false;
             l++;
             r--;
         }
