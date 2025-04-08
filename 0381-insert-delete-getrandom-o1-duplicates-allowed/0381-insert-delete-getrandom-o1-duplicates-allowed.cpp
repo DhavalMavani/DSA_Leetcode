ea71e85bbdb7c5dacdf4803200751bd9db9934f3
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-08 11:30:27 +0530

    Time: 35 ms (93.05%), Space: 110.4 MB (90.95%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,37 +1,41 @@
-//Solution 01:
 class RandomizedCollection {
 public:
-    
-    vector<pair<int, int>>a;
-    unordered_map<int, vector<int>> mp;
+    unordered_map<int,priority_queue<int>> ump;
+    vector <int> value;
     
     bool insert(int val) {
-        bool isPresent=!mp.count(val);
-        mp[val].push_back(a.size());
-        a.push_back({val, mp[val].size()-1});
+        bool isPresent=!ump.count(val);
+        ump[val].emplace(value.size());
+        value.emplace_back(val);
         return isPresent;
     }
     
     bool remove(int val) {
-        if(mp.count(val)){
-            pair<int, int> last = a.back();
-            mp[last.first][last.second] = mp[val].back();
-            a[mp[val].back()] = last;
-            mp[val].pop_back();
-            if(mp[val].empty()) mp.erase(val);
-            a.pop_back();
+        if( ump.count(val) ){
+            
+            int i= ump[val].top();
+
+            if(ump[val].size()==1)ump.erase(val);
+            else ump[val].pop();
+
+            if(i<value.size()-1){
+                value[i] = value.back();
+                ump[value[i]].pop();
+                ump[value[i]].emplace(i);
+            }
+            value.pop_back();
             return true;
         }
-        return false;
-         
+        return false;        
     }
     
     int getRandom() {
-        return a[rand()%a.size()].first;
+        return value[rand()%value.size()];
     }
 };
 
-/* * Your RandomizedCollection object will be instantiated and called as such:
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
  * RandomizedCollection* obj = new RandomizedCollection();
  * bool param_1 = obj->insert(val);
  * bool param_2 = obj->remove(val);
