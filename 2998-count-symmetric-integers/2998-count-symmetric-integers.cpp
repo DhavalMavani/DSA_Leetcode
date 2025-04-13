301336a4bf4c8182c15a0d22b4ce81a9fa28dc7c
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-13 12:36:36 +0530

    Time: 71 ms (43.78%), Space: 9.7 MB (47.76%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -4,7 +4,10 @@ public:
         int count = 0;
         for (int i = low; i <= high; i++) {
             string s = to_string(i);
-            if (s.size() % 2 == 0 && accumulate(s.begin(), s.begin() + s.size()/2, 0) == accumulate(s.begin() + s.size()/2, s.end(), 0)) count++;
+            if (s.size() % 2 == 0 && 
+            accumulate(s.begin(), s.begin() + s.size()/2, 0) ==
+            accumulate(s.begin() + s.size()/2, s.end(), 0))
+                count++;
         }
         return count;
     }
