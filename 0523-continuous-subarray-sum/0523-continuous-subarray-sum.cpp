2acb9c1c9795115660d914b6738f70cf11d4001e
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-15 11:44:19 +0530

    Time: 81 ms (31.14%), Space: 146.4 MB (41.69%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -5,12 +5,13 @@ public:
         
         unordered_map<int,int> ump;
         ump[0]=-1;
+        
         for (int i = 0; i < n; i++) {
             sum += nums[i];
             int curr = sum%k;
 
             if (ump.count(curr) ){
-                if( i-ump[curr]>=2) return true;
+                if(i-ump[curr]>=2) return true;
             } 
             else  ump[curr]=i;
         }
