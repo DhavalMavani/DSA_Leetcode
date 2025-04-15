2b2d5bba2145e60e02f64bc9b08b15e4222c1088
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-15 15:46:51 +0530

    Time: 3 ms (11.04%), Space: 9.8 MB (5.18%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool helper(int n, bool alice,vector<vector<int>> &dp){
+        if(n<=1) return alice;
+
+        if(dp[n][alice]!=-1) return dp[n][alice];
+        int lim=sqrt(n);
+
+        for(int i=1;i<=lim;i++){
+            if(n%i) continue;
+            if(alice){
+                if(!helper(n-i,!alice,dp)) return dp[n][alice]=false;
+                return dp[n][alice]=true;
+            } 
+            else{
+                if( helper(n-i,!alice,dp) ) return dp[n][alice]=true;
+                return dp[n][alice]=false;
+            } 
+        }
+        return false;
+    }
+    bool divisorGame(int n) {
+        vector<vector<int>> dp(n+1,vector<int> (2,-1));
+
+        return !helper(n, true,dp);
+    }
+};
