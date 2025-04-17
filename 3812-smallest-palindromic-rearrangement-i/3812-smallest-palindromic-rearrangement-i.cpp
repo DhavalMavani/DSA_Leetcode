c226ee9206d09b3c831691e32f9e4c43cf90346c
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-17 23:11:29 +0530

    Time: 87 ms (44.95%), Space: 60.8 MB (89.44%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
@@ -3,19 +3,21 @@ public:
     string smallestPalindrome(string s) {
         vector<int> freq(26,0);
         for(auto &i: s) freq[i-'a']++;
-        int oddChar=-1;
-        string ans;
+
+        int l=0,n=s.size();
+        string ans(n,'*');
+
         for(int i=0;i<26;i++){
-            if(freq[i]%2==1) oddChar=i;
-
-            ans+=string(freq[i]/2, 'a'+i);
-            freq[i]-= freq[i]/2;
+            if(freq[i]==0) continue;
+            cout<<char('a'+i)<<" "<<freq[i]<<" \n";
+            for(int j=0;j<freq[i]/2;j++,l++){
+                ans[l]='a'+i;
+                ans[n-1-l]='a'+i;
+            }
+            if(freq[i]%2) ans[n/2]='a'+i;
         }
-        string back=ans;
-        reverse(back.begin(),back.end());
-        
-        if(oddChar!=-1) ans+='a'+oddChar;
 
-        return ans+back;
+
+        return ans;
     }
 };
