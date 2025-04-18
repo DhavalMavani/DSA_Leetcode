d43ffc34cc7ae89618a9953f0d2bc23da1d45a49
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-18 17:12:54 +0530

    Time: 9 ms (49.87%), Space: 17.7 MB (52.6%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int findMaxProfit(vector<int>& prices,int numTransaction,int index,vector<vector<int>>& dp){
-      if(index>=prices.size() || numTransaction==0 ){
-        return 0;
-      }
+      if(index>=prices.size() || numTransaction==0 ) return 0;
 
-      if(dp[index][numTransaction]!=-1){
-        return dp[index][numTransaction];
-      }
-      int a=0,b=0;
+      if(dp[index][numTransaction]!=-1) return dp[index][numTransaction];
+
       if(numTransaction%2==0){
-        a =  -prices[index] + findMaxProfit(prices,numTransaction-1,index+1,dp);
-        b =  findMaxProfit(prices,numTransaction,index+1,dp);
-        
+        int a =  -prices[index] + findMaxProfit(prices,numTransaction-1,index+1,dp);
+        int b =  findMaxProfit(prices,numTransaction,index+1,dp);
+        return dp[index][numTransaction] = max(a,b);
       }
       else{
-        a = prices[index] + findMaxProfit(prices,numTransaction-1,index+1,dp);
-        b = findMaxProfit(prices,numTransaction,index+1,dp);
+        int a = prices[index] + findMaxProfit(prices,numTransaction-1,index+1,dp);
+        int b = findMaxProfit(prices,numTransaction,index+1,dp);
+        return dp[index][numTransaction] = max(a,b);
       }
-      return dp[index][numTransaction] = max(a,b);
+      return 0;
     }
     int maxProfit(int k, vector<int>& prices) {
       vector<vector<int>> dp(prices.size(),vector<int>(2*k+1,-1));
