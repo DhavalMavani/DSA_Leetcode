46434b7aa565ce43e312798a20f602db9d05d5f0
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-20 13:51:06 +0530

    Time: 30 ms (12.38%), Space: 212.8 MB (7.04%) - LeetHub

--- 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
         stack<int> st;
-        for (int &x : nums) {
-            int mx = x;
-            while (!st.empty() && st.top() > mx) {
-                mx = st.top();
-                st.pop();
-            }
-            st.push(mx);
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--) {
+            int curr = nums[i];
+            while (!st.empty() && st.top() < curr) st.pop();
+            st.push(curr);
         }
         return st.size();
     }
