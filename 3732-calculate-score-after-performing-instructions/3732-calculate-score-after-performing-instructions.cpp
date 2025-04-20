308ba1a6b5d72db3cc658fa2cba972044f897980
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-20 11:42:43 +0530

    Time: 7 ms (47.46%), Space: 167.1 MB (78.99%) - LeetHub

--- 3732-calculate-score-after-performing-instructions/3732-calculate-score-after-performing-instructions.cpp
+++ 3732-calculate-score-after-performing-instructions/3732-calculate-score-after-performing-instructions.cpp
@@ -3,10 +3,10 @@ public:
     long long calculateScore(vector<string>& instructions, vector<int>& values) {
         int n = instructions.size(), i = 0;
         long long score = 0;
-        unordered_set<int> visited;
+        vector<bool> visited(n,false);
 
-        while (i>=0 && i<n && !visited.count(i)) {
-            visited.emplace(i);
+        while (i>=0 && i<n && !visited[i] ) {
+            visited[i]=true;
             if (instructions[i] == "add") {
                 score += values[i];
                 i++;
