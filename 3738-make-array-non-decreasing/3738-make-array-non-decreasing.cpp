2df0fc2ed8ab5e728d52d66320d38fe5607abef2
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-20 13:51:44 +0530

    Time: 45 ms (5.98%), Space: 212.8 MB (7.04%) - LeetHub

--- 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
@@ -4,9 +4,8 @@ public:
         stack<int> st;
         int n=nums.size();
         for(int i=n-1;i>=0;i--) {
-            int curr = nums[i];
-            while (!st.empty() && st.top() < curr) st.pop();
-            st.push(curr);
+            while (!st.empty() && st.top() < nums[i]) st.pop();
+            st.push(nums[i]);
         }
         return st.size();
     }
