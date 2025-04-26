72dea4b0c6f8610345319b36eeaf7e1a90dec5d0
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2025-04-26 11:46:15 +0530

    Time: 60 ms (47.12%), Space: 122.8 MB (59.16%) - LeetHub

--- 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
-        long long ans = 0;
-        int n = nums.size();
-        for(int i = 0; i < n;i++){
-            if (nums[i] % modulo == k) nums[i] = 1;
-            else nums[i] = 0;
+    long long countInterestingSubarrays(vector<int>& nums, int m, int k) {
+        long long ans=0;
+        int n=nums.size(), prefixSum=0;
+        unordered_map<int,int> ump;
+        ump[0]=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]%m==k) prefixSum++;
+
+            int r1=prefixSum % m ;
+            int r2=(r1-k+m)%m;
+
+            ans+=ump[r2];
+            ump[r1]++;
         }
 
-        unordered_map <int, int> mp;
-        int total = 0;
-        for(int i = 0; i < n;i++){
-            total += nums[i];
-            int mod_val = total % modulo;
-            if (mod_val == k) ans++;
-            int find = mod_val - k;
-            if (find < 0) find += modulo;
-            ans += mp[find];
-            mp[mod_val]++;
-        }
         return ans;
     }
 };
